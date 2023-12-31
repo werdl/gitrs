3079a530eb3eec4dd76be686eb45dc00603560fa
werdl <werdl_is_cool@outlook.com>
2023-12-31 09:29:54 +0000

    add some more test

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,29 @@
 include!("core.rs");
 
+
 #[cfg(test)]
 mod tests {
-    // Tests are in doc comments in core.rs
+    use super::*;
+
+    #[test]
+    fn test_git_run() {
+        let git = Git::new(vec!["--version"]);
+        let result = git.run();
+
+        assert!(result.is_ok(), "Expected Ok, got Err");
+
+        assert!(result.stdout().is_some(), "Expected stdout, got None");
+        assert_eq!(result.code(), 0, "Expected exit code 0, got {}", result.code());
+    }
+
+    #[test]
+    fn test_git_stream() {
+        let git = Git::new(vec!["--version"]);
+        let result = git.stream();
+
+        assert!(result.is_ok(), "Expected Ok, got Err");
+
+        assert!(result.stdout().is_none(), "Expected None, got Some");
+        assert_eq!(result.code(), 0, "Expected exit code 0, got {}", result.code());
+    }
 }
